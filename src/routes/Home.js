3d4ab43208b4d9ff0a7ffb93e2c58b684a3cb31a
rishisankar <rishi.sankar@gmail.com>
2020-07-05 22:10:06 -0700

    About panel on homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -17,14 +17,27 @@ class Home extends Component {
                     valign="65%"
                 />
 
-                content
-                <br/> content
-                <br/> content
-                <br/> content
-                <br/> content
-                <br/> content
-                <br/> content
-                <br/> content
+                <div className="about-panel">
+                    <div className="about-text">
+                        <div className="about-heading">about us</div>
+                        <div className="about-description">
+                            <p>
+                                The Regents Scholar Society is composed of students with 
+                                different identities, interests, and passions. Whether 
+                                north campus or south campus, pre-med or business, scientists 
+                                or artists, the society transcends these divisions through 
+                                our shared commitment to scholarship, leadership, and service.
+                            </p>
+                            <p>
+                                We strive to support our members and the local community
+                                through our various internal and external programs, including
+                                service events, leadership opportunities, and the Los Angeles
+                                Mentorship Program.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="about-img-wrapper" />
+                </div>
             </RSSPage>
         )
     }
