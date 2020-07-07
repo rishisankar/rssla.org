0fc9b64f3bcb1f9512447493494e1fc5693f102b
rishisankar <rishi.sankar@gmail.com>
2020-07-06 23:16:59 -0700

    Nav dropdown & home about panel resize

--- src/util/Header.js
+++ src/util/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 import './header.css'
 
@@ -13,7 +14,15 @@ class Header extends Component {
                     <img src="/images/rss_logo.png" alt="rss logo" className="header-logo"/>
                 </div>
                 <div className="nav-bar-wrapper">
-                    <NavBox label="about" link="about" />
+                    <ExpandableNavBox 
+                        label="about" 
+                        link={null} 
+                        options={[{label: "programs", to: "programs"},
+                                  {label: "committees", to: "committees"},
+                                  {label: "board", to: "board"},
+                                  {label: "sponsors", to: "sponsors"}
+                                ]}
+                    />
                     <NavBox label="gallery" link="gallery" />
                     <NavBox label="prospectives" link="prospectives" />
                     <NavBox label="contact us" link="contact" />
@@ -27,8 +36,52 @@ class Header extends Component {
 class NavBox extends Component {
     render() {
         return (
+            <Link 
+                    className="nav-box-link"
+                    to={this.props.link}
+                >
             <div className="nav-box">
+                
+                    {this.props.label}
+            </div>
+                </Link>
+        )
+    }
+}
+
+class ExpandableNavBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    setOpen(open) {
+        this.setState({open: open});
+    }
+
+    render() {        
+        return (
+            <div className="nav-box expandable" 
+                onMouseEnter={() => this.setOpen(true)}
+                onMouseLeave={() => this.setOpen(false)}    
+            >
                 {this.props.label}
+                &nbsp;
+                <i class="navicon fas fa-chevron-down"/>
+                
+                {this.state.open ?
+                    <div className="nav-box-expanded">
+                        {this.props.options.map((item, i) =>
+                            <Link className="nav-box-link" to={item.to}>
+                                <div className="nav-box-expanded-item">
+                                    {item.label}
+                                </div>
+                            </Link>
+                        )}
+                    </div>
+                : null}
             </div>
         )
     }
