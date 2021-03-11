080624a2c91c381767af9fa8b6ed5a63be12ad87
Rishi Sankar <rishi.sankar@gmail.com>
2021-03-10 17:07:27 -0800

    we are rss panel mobile version

--- src/routes/Prospectives.js
+++ src/routes/Prospectives.js
@@ -8,6 +8,27 @@ import {BenefitDescription, BenefitWrapper} from '../util/BenefitDescription';
 import './prospectives.css';
 
 class Prospectives extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: window.innerWidth
+        }
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth });
+    }
+
     render() {
         return (
             <RSSPage className="prospectives">
@@ -18,6 +39,60 @@ class Prospectives extends Component {
                     valign="65%"
                 />
 
+                <ProspectivesIntro width={this.state.width} />
+
+                { /* Daniel */ }
+                <div className="welcome_wrapper">
+                    <h2 className= "welcome_header"> welcome to our overnight stay program!</h2> 
+                    <div className= "welcome_to_osp">
+                        Congratulations on receiving the Regents Scholarship! The overnight stay program (OSP) provides prospectives a valuable opportunity to learn 
+                        more about the Regents Scholar Society and UCLA. This year, the RSS OSP Coordinators have planned an interactive online experience.
+                        A key aspect of every OSP includes Q&amp;A panels for academics and campus life, social activities, and more. In order to get the full OSP experience, 
+                        we highly recommend you arrange your schedule to let you attend the session from beginning to end. We're excited to see you! 
+                    </div>
+                </div>
+                <div className= "welcome_to_osp_image">
+                    <img src="/images/prospectives/osp mountain cropped.jpg" alt="osp banner" className= "osp_image"/>
+                </div>
+               
+                { /* Rishi */ }
+                <TextPanel heading="osp 1" id="osp_panel">
+                    <div className="osp_description">
+                        month 00 - month 00
+                    </div>
+                    <div className="itinerary_wrapper">
+                        itinerary
+                    </div>
+                </TextPanel>
+
+                { /* Ray */ }
+                <TextPanel heading="meet your coordinators!">
+                asdf
+                </TextPanel>
+
+                { /* will copy/paste osp 1 stuff for osp 2 after we finish */ }
+
+                { /* Christian */ }
+                <TextPanel heading="contact us!">
+                    asdf
+                </TextPanel>
+            </RSSPage>
+        )
+    }
+}
+
+class ProspectivesIntro extends Component {
+    render() {
+        if (this.props.width >= 900) {
+            return this.renderLarge();
+        } else {
+            return this.renderSmall();
+        }
+    }
+
+    renderLarge() {
+        return (
+            <React.Fragment>
                 { /* Brandon */ }
                 <TextPanel heading="we are rss" id="we-are-rss-panel">
                     <div className="we_are_rss_wrapper">
@@ -58,61 +133,89 @@ class Prospectives extends Component {
                             </BenefitWrapper>
                         </div>
                         <div className='letter_wrapper'>
-                            <span style={{fontSize: "calc(20px + 1.1vw)", fontFamily: "Kollektif"}}>a letter from our external vice president</span>
-                            <br /><br />
-                            Dear prospective RSSer,
-                            <br /><br />
-                            Congratulations on your acceptance to UCLA as a Regents Scholar, and welcome to UCLA RSS and OSP!
-                            We are extremely excited to have you in our OSP, and can't wait to meet you virtually.
-                            In these three days, you'll learn what college, UCLA, and RSS are all about — on top of meeting 
-                            some lifelong friends and even potentially future roommates! If you have any questions about college, 
-                            and I mean any — this is the place to have them answered. We look forward to meeting you soon!
-                            <br /> <br />
-                            - &nbsp;&nbsp;&nbsp;&nbsp;<span style={{fontSize: "calc(50px + 1.1vw)", fontFamily: "Cambria"}}>Rebecca Zhu</span>
-                            <br />
-                            External Vice President
+                            <div className="evp_letter_header">
+                                a letter from our external vice president
+                            </div>
+                            <div className="evp_letter_body">
+                                Dear prospective RSSer,
+                                <br/><br/>
+                                Congratulations on your acceptance to UCLA as a Regents Scholar, and welcome to UCLA RSS and OSP!
+                                We are extremely excited to have you in our OSP, and can't wait to meet you virtually.
+                                <br/><br/>
+                                In these three days, you'll learn what college, UCLA, and RSS are all about — on top of meeting 
+                                some lifelong friends and even potentially future roommates! If you have any questions about college, 
+                                and I mean any — this is the place to have them answered. We look forward to meeting you soon!
+                            </div>
+                            <div className="evp_letter_signature">- Rebecca Zhu</div>
+                            <div className="evp_letter_title">
+                                External Vice President
+                            </div>
                         </div>
                     </div>
                 </TextPanel>
-
-                { /* Daniel */ }
-                <div className="welcome_wrapper">
-                    <h2 className= "welcome_header"> welcome to our overnight stay program!</h2> 
-                    <div className= "welcome_to_osp">
-                        Congratulations on receiving the Regents Scholarship! The overnight stay program (OSP) provides prospectives a valuable opportunity to learn 
-                        more about the Regents Scholar Society and UCLA. This year, the RSS OSP Coordinators have planned an interactive online experience.
-                        A key aspect of every OSP includes Q&amp;A panels for academics and campus life, social activities, and more. In order to get the full OSP experience, 
-                        we highly recommend you arrange your schedule to let you attend the session from beginning to end. We're excited to see you! 
-                    </div>
-                </div>
-                <div className= "welcome_to_osp_image">
-                    <img src="/images/prospectives/osp mountain cropped.jpg" alt="osp banner" className= "osp_image"/>
-                </div>
-               
-                { /* Rishi */ }
-                <TextPanel heading="osp 1" id="osp_panel">
-                    <div className="osp_description">
-                        month 00 - month 00
-                    </div>
-                    <div className="itinerary_wrapper">
-                        itinerary
-                    </div>
-                </TextPanel>
-
-                { /* Ray */ }
-                <TextPanel heading="meet your coordinators!">
-                asdf
-                </TextPanel>
-
-                { /* will copy/paste osp 1 stuff for osp 2 after we finish */ }
-
-                { /* Christian */ }
-                <TextPanel heading="contact us!">
-                    asdf
-                </TextPanel>
-            </RSSPage>
+            </React.Fragment>
         )
     }
+
+    renderSmall() {
+        return (
+            <React.Fragment>
+                <TextPanel heading="we are rss" id="we-are-rss-panel">
+                    The Regents Scholar Society at UCLA, or RSS, offers membership to everyone who has received the
+                    Regents Scholarship.
+                    The scholarship is offered to those at the top of each incoming class, but 
+                    we're more than just smart -- we're kind, ambitious, passionate, funny, driven, and 
+                    maybe a little eccentric. Most importantly, we're a diverse group who are here to 
+                    welcome you to UCLA with open arms and open hearts.
+                    <br /><br />
+                    The Regents Scholarship is awarded to up to 100 incoming freshmen 
+                    and transfer students. Alongside membership in the Regents Scholar Society, 
+                    all Regents Scholars receive:
+                    <BenefitWrapper>
+                        <BenefitDescription
+                            heading="$2000"
+                            subheading="honorarium"
+                            desc="A $2,000 honorarium, awarded quarterly for four years (or two years for transfers).
+                                Additionally, university scholarships or grants to cover the student's financial need."
+                            bgr="openbook.png"
+                        />
+                        <BenefitDescription
+                            heading="13"
+                            subheading="units"
+                            desc="Priority enrollment in 13 class units, starting in their second quarter."
+                            bgr="pencil.png"
+                            scale="90%"
+                        />
+                        <BenefitDescription
+                            heading="4"
+                            subheading="years"
+                            desc="Four years of university housing upon submission of the Housing application. 
+                                Additionally, a parking pass upon submission of the Parking application."
+                            bgr="house.png"
+                            scale="90%"
+                        />
+                    </BenefitWrapper>
+                </TextPanel>
+                <TextPanel heading="a letter from our external vice president" id="small-letter-panel">
+                    <div className="evp_letter_body">
+                        Dear prospective RSSer,
+                        <br/><br/>
+                        Congratulations on your acceptance to UCLA as a Regents Scholar, and welcome to UCLA RSS and OSP!
+                        We are extremely excited to have you in our OSP, and can't wait to meet you virtually.
+                        <br/><br/>
+                        In these three days, you'll learn what college, UCLA, and RSS are all about — on top of meeting 
+                        some lifelong friends and even potentially future roommates! If you have any questions about college, 
+                        and I mean any — this is the place to have them answered. We look forward to meeting you soon!
+                    </div>
+                    <div className="evp_letter_signature">- Rebecca Zhu</div>
+                    <div className="evp_letter_title">
+                        External Vice President
+                    </div>
+                </TextPanel>
+            </React.Fragment>
+        )
+    }
+
 }
 
 export default Prospectives;
