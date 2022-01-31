cfd81a048c11e4ae8363e99f73a4f13aeca7352e
David Deng <daviddeng8@gmail.com>
2022-01-31 10:46:41 -0800

    Added banner image to service page.

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import Banner from '../util/Banner';
+import RSSPage from '../util/RSSPage';
 
 class Service extends Component {
     render() {
         return (
-            <h1> Service </h1>
+            <RSSPage>
+                <Banner 
+                    text="service"
+                    bgr="/images/banners/2021_beachcleanup.jpeg"
+                    valign="66%"
+                />
+            </RSSPage>
         )
     }
 }
